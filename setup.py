b8632e84aadc754fbe367f76c52d03de38aa4b11
LordMathis <matus@namesny.com>
2023-12-13 22:51:19 +0100

    Add init

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="mlflow-plugin-proxy-auth",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     install_requires=["mlflow"],
     entry_points={
